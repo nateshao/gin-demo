7065b3f714b2389912ca7324fa9a34a93e5cdb13
nateshao <shaotongjie@gmail.com>
2021-05-03 15:30:01 +0800

    gorm-mysql-crud

--- lesson19-gorm/main.go
+++ lesson19-gorm/main.go
@@ -21,33 +21,18 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer db.Close()
 
-	// 创建表，自动迁移（把结构体和数据表进行对应）
-	db.AutoMigrate(&UserInfo{})
+	// 创建表 自动迁移（把结构体与数据表进行对应）
+	db.AutoMigrate(UserInfo{})
 
-	u1 := UserInfo{1, "七米", "男", "篮球"}
-	u2 := UserInfo{2, "沙河娜扎", "女", "足球"}
-	u3 := UserInfo{3, "千羽", "男", "Go开发"}
-
-	// 创建记录
-	db.Create(&u1)
-	db.Create(&u2)
-	db.Create(&u3)
 	// 查询
-	var u = new(UserInfo)
-	db.First(u)
-	fmt.Printf("%#v\n", u)
-
-	var uu UserInfo
-	db.Find(&uu, "hobby=?", "足球")
-	fmt.Printf("%#v\n", uu)
+	var u UserInfo
+	db.First(&u)
+	fmt.Printf("u:%#v\n", u)
 
 	// 更新
 	db.Model(&u).Update("hobby", "双色球")
+
 	// 删除
 	db.Delete(&u)
 }
