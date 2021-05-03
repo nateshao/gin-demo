2cdda162142b18986ee075fa5b277c8f44f56ef6
nateshao <shaotongjie@gmail.com>
2021-05-03 15:15:11 +0800

    gorm

--- lesson19-gorm/main.go
+++ lesson19-gorm/main.go
@@ -21,15 +21,22 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer db.Close()
 
-	// 自动迁移
+	// 创建表，自动迁移（把结构体和数据表进行对应）
 	db.AutoMigrate(&UserInfo{})
 
 	u1 := UserInfo{1, "七米", "男", "篮球"}
 	u2 := UserInfo{2, "沙河娜扎", "女", "足球"}
+	u3 := UserInfo{3, "千羽", "男", "Go开发"}
+
 	// 创建记录
 	db.Create(&u1)
 	db.Create(&u2)
+	db.Create(&u3)
 	// 查询
 	var u = new(UserInfo)
 	db.First(u)
