21825bbf3c900b776d40079832ee070d062d3530
nateshao <shaotongjie@gmail.com>
2021-05-05 18:15:52 +0800

    gorm-model

--- lesson20-gorm-model/main.go
+++ lesson20-gorm-model/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"database/sql"
+	"time"
+)
+
+// 定义模型
+type User struct {
+	gorm.Model   // 内嵌gorm.Model
+	Name         string
+	Age          sql.NullInt64 // 零值类型
+	Birthday     *time.Time
+	Email        string  `gorm:"type:varchar(100);unique_index"`
+	Role         string  `gorm:"size:255"`        // 设置字段大小为255
+	MemberNumber *string `gorm:"unique;not null"` // 设置会员号（member number）唯一并且不为空
+	Num          int     `gorm:"AUTO_INCREMENT"`  // 设置 num 为自增类型
+	Address      string  `gorm:"index:addr"`      // 给address字段创建名为addr的索引
+	IgnoreMe     int     `gorm:"-"`               // 忽略本字段
+}
+
 func main() {
 
 }
