220a611144d6f3fc26c6ef832815cbde962c802c
nateshao <shaotongjie@gmail.com>
2024-01-07 14:44:11 +0800

    init

---
+++ gin-demo-25-bubble-v2/web/README.md
@@ -0,0 +1,26 @@
+# bubble-frontend
+
+This project is the frontend code base vue2.0 for [bubble](https://github.com/Q1mi/bubble).
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ gin-demo-25-bubble-v2/web/src/views/Index.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-container>
+    <el-header>公众号：程序员千羽</el-header>
+    <el-main>
+      <el-row type="flex" justify="center">
+        <el-col :xs="20" :span="12" >
+          <div class="grid-content">
+            <el-divider>
+              <h1>TODO备忘录</h1>
+            </el-divider>
+            <TodoList></TodoList>
+          </div>
+        </el-col>
+      </el-row>
+    </el-main>
+    <el-footer>~~求关注~~</el-footer>
+  </el-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import TodoList from "@/components/TodoList.vue";
+
+export default {
+  name: "Index",
+  components: {
+    TodoList
+  }
+};
+</script>
+<style>
+.el-header,
+.el-footer {
+  background-color: #409eff;
+  color: #fff;
+  text-align: center;
+  line-height: 60px;
+}
+.el-footer {
+  background-color: #909399;
+  display: block;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+}
+</style>
